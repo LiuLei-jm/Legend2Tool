3a285173ab7f5378f5d93009526d95a375f48116
LiuLei-jm <14699396@qq.com>
2025-05-26 17:08:45 +0800

    添加项目文件。

---
+++ src/Legend2Tool.WPF/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace Legend2Tool.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Services = ConfigureServices();
+
+            this.InitializeComponent();
+        }
+
+        public static new App Current => (App)Application.Current;
+        public IServiceProvider Services { get; }
+
+        private static IServiceProvider? ConfigureServices()
+        {
+            var services = new ServiceCollection();
+            return services.BuildServiceProvider();
+        }
+    }
+}
---
+++ src/Legend2Tool.WPF/Views/PortConfView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Legend2Tool.WPF.Views
+{
+    /// <summary>
+    /// PortConfView.xaml 的交互逻辑
+    /// </summary>
+    public partial class PortConfView : UserControl
+    {
+        public PortConfView()
+        {
+            InitializeComponent();
+        }
+    }
+}
